765eaa72f997e96b8b61d03e5c88fe44bc7202b9
Moon-YJ <tunjeong@gmail.com>
2024-01-24 15:53:55 +0900

    framer-motion으로 모션 적용(router 변경을 trigger로 설정)

--- components/template/layout/Layout.jsx
+++ components/template/layout/Layout.jsx
@@ -4,6 +4,7 @@ import Header from '@/components/organisms/header/Header';
 import Breadcrumb from '@/components/molecules/breadcrumb/Breadcrumb';
 import { Nanum_Myeongjo, Orbitron } from 'next/font/google';
 import { useRouter } from 'next/router';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const nanum = Nanum_Myeongjo({
 	subsets: ['latin'],
@@ -19,15 +20,28 @@ const orbitron = Orbitron({
 	variable: '--font-orbitron'
 });
 
-export default function Layout({ children, pageType }) {
+export default function Layout({ children }) {
 	const router = useRouter();
 	return (
-		<div className={clsx(styles.layout, nanum.variable, orbitron.variable)}>
-			<Header />
-			<div className={clsx(styles.content, styles[pageType])}>
-				{router.asPath !== '/' && <Breadcrumb />}
-				{children}
-			</div>
-		</div>
+		// mode='wait' => 동작되고 있는 모션 컴포넌트가 있으면 해당 모션이 끝날때까지 unmount를 지연해서 사라지는 모션 시간 확보
+		<AnimatePresence mode='wait'>
+			{/* router 변경을 감지하는 모션 컴포넌트 */}
+			<motion.div key={router.pathname}>
+				<div className={clsx(styles.layout, nanum.variable, orbitron.variable)}>
+					<Header />
+					<div className={clsx(styles.content)}>
+						{router.asPath !== '/' && <Breadcrumb />}
+						{children}
+					</div>
+				</div>
+				{/* router 변경할때마다 모션을 적용할 박스 요소 */}
+				<motion.div className='in' initial={{ scaleX: 0 }} animate={{ scaleX: 0 }} exit={{ scaleX: 1 }} transition={{ duration: 0.5 }}></motion.div>
+				<motion.div className='out' initial={{ scaleX: 1 }} animate={{ scaleX: 0 }} exit={{ scaleX: 0 }} transition={{ duration: 0.5 }}></motion.div>
+
+				{/* scaleY 모션 */}
+				{/* <motion.div className='in' initial={{ scaleY: 0 }} animate={{ scaleY: 0 }} exit={{ scaleY: 1 }} transition={{ duration: 0.5 }}></motion.div>
+				<motion.div className='out' initial={{ scaleY: 1 }} animate={{ scaleY: 0 }} exit={{ scaleY: 0 }} transition={{ duration: 0.5 }}></motion.div> */}
+			</motion.div>
+		</AnimatePresence>
 	);
 }
