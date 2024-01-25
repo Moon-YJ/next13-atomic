45ed962dd9ea70a06876074904dc3ac264c78d17
Moon-YJ <tunjeong@gmail.com>
2024-01-25 17:02:33 +0900

    Footer컴포넌트 추가 및 테마컬러 전역으로 관리

--- components/template/layout/Layout.jsx
+++ components/template/layout/Layout.jsx
@@ -5,6 +5,8 @@ import Breadcrumb from '@/components/molecules/breadcrumb/Breadcrumb';
 import { Nanum_Myeongjo, Orbitron } from 'next/font/google';
 import { useRouter } from 'next/router';
 import { AnimatePresence, motion } from 'framer-motion';
+import { useGlobalData } from '@/hooks/useGlobalContext';
+import Footer from '@/components/organisms/footer/Footer';
 
 const nanum = Nanum_Myeongjo({
 	subsets: ['latin'],
@@ -22,27 +24,34 @@ const orbitron = Orbitron({
 
 export default function Layout({ children }) {
 	const router = useRouter();
+	const { Theme } = useGlobalData();
 	return (
 		// mode='wait' => 동작되고 있는 모션 컴포넌트가 있으면 해당 모션이 끝날때까지 unmount를 지연해서 사라지는 모션 시간 확보
 		<AnimatePresence mode='wait'>
 			{/* router 변경을 감지하는 모션 컴포넌트 */}
 			<motion.div key={router.pathname}>
-				<div className={clsx(styles.layout, nanum.variable, orbitron.variable)}>
+				<div className={clsx(styles.layout, nanum.variable, orbitron.variable, Theme)}>
 					<Header />
 					<div className={clsx(styles.content)}>
 						{router.asPath !== '/' && <Breadcrumb />}
 						{children}
 					</div>
-				</div>
-				{/* router 변경할때마다 모션을 적용할 박스 요소 */}
-				{/* 패널이 안보이다가 왼쪽에서 오른쪽으로 늘어나는 모션 컴포넌트 */}
-				<motion.div className='in' initial={{ scaleX: 0 }} animate={{ scaleX: 0 }} exit={{ scaleX: 1 }} transition={{ duration: 0.5 }}></motion.div>
-				{/* 패널이 보이다가 오른쪽으로 사라지는 모션 컴포넌트 */}
-				<motion.div className='out' initial={{ scaleX: 1 }} animate={{ scaleX: 0 }} exit={{ scaleX: 0 }} transition={{ duration: 0.5 }}></motion.div>
+					{/* router 변경할때마다 모션을 적용할 박스 요소 */}
+					{/* 패널이 안보이다가 왼쪽에서 오른쪽으로 늘어나는 모션 컴포넌트 */}
+					<motion.div className='in' initial={{ scaleX: 0 }} animate={{ scaleX: 0 }} exit={{ scaleX: 1 }} transition={{ duration: 0.5 }}></motion.div>
+					{/* 패널이 보이다가 오른쪽으로 사라지는 모션 컴포넌트 */}
+					<motion.div
+						className='out'
+						initial={{ scaleX: 1 }}
+						animate={{ scaleX: 0 }}
+						exit={{ scaleX: 0 }}
+						transition={{ duration: 0.5 }}></motion.div>
 
-				{/* scaleY 모션 */}
-				{/* <motion.div className='in' initial={{ scaleY: 0 }} animate={{ scaleY: 0 }} exit={{ scaleY: 1 }} transition={{ duration: 0.5 }}></motion.div>
-				<motion.div className='out' initial={{ scaleY: 1 }} animate={{ scaleY: 0 }} exit={{ scaleY: 0 }} transition={{ duration: 0.5 }}></motion.div> */}
+					{/* scaleY 모션 */}
+					{/* <motion.div className='in' initial={{ scaleY: 0 }} animate={{ scaleY: 0 }} exit={{ scaleY: 1 }} transition={{ duration: 0.5 }}></motion.div>
+					<motion.div className='out' initial={{ scaleY: 1 }} animate={{ scaleY: 0 }} exit={{ scaleY: 0 }} transition={{ duration: 0.5 }}></motion.div> */}
+					<Footer />
+				</div>
 			</motion.div>
 		</AnimatePresence>
 	);
